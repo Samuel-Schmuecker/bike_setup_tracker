c15c5ad5de6f9227eebe04a333d808e5ef1f0f29
Samuel Schmuecker <Samuel.Schmuecker@gmail.com>
2026-08-23 23:02:34 +0200

    web app fix 3

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-// Importiere deinen Provider und den HomeScreen
 import 'providers/bike_provider.dart';
 import 'screens/home/home_screen.dart';
 
 void main() {
-  // Wichtig für Web und SharedPreferences, bevor die App startet
   WidgetsFlutterBinding.ensureInitialized(); 
 
   runApp(
-    // Provider an der obersten Stelle der App registrieren
     ChangeNotifierProvider(
       create: (context) => BikeProvider(),
       child: const MyApp(),
@@ -27,26 +24,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Bike Setup Tracker',
-      debugShowCheckedModeBanner: false, // Entfernt das rote Debug-Banner
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // Wir haben die App optisch eher dunkel/hochwertig gestaltet,
-        // daher setzen wir das Grund-Theme auf dunkel.
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.teal, // Deine Akzentfarbe (kannst du anpassen)
+          seedColor: Colors.teal,
           brightness: Brightness.dark, 
         ),
         useMaterial3: true,
         
-        // Hier lag der Fehler! Die saubere Einbindung der PageTransitions:
-        pageTransitionsTheme: PageTransitionsTheme(
-          builders: <TargetPlatform, PageTransitionsBuilder>{
-            TargetPlatform.android: CupertinoPageTransitionsBuilder(),
-            TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
-            TargetPlatform.macOS: CupertinoPageTransitionsBuilder(),
-            TargetPlatform.windows: FadeUpwardsPageTransitionsBuilder(),
-            TargetPlatform.linux: FadeUpwardsPageTransitionsBuilder(),
-          },
-        ),
+        // Den problematischen "pageTransitionsTheme"-Block haben wir hier 
+        // komplett entfernt. Flutter erledigt das jetzt automatisch und 
+        // 100% kompatibel für das Web!
       ),
       home: const HomeScreen(),
     );
