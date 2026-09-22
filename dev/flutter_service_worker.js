0122fcb1322faa1e33137e5dd73f682088631bed
Samuel-Schmuecker <Samuel-Schmuecker@users.noreply.github.com>
2026-09-22 09:04:13 +0000

    deploy: d552164b0e9267d81e7aa5b9092abc75caf5f91e

--- dev/flutter_service_worker.js
+++ dev/flutter_service_worker.js
@@ -6,7 +6,7 @@ const CACHE_NAME = 'flutter-app-cache' + ":" + self.registration.scope;
 const RESOURCES = {"manifest.json": "f1eef43042f06f6e47494730385f7104",
 "version.json": "553994d41335e565977f17b9d4e5d49e",
 "main.dart.js": "f2422a36ba9da9cface0491695cf02c5",
-"flutter_bootstrap.js": "57334b2516bc5aae974b82e69aef5fd9",
+"flutter_bootstrap.js": "a5fb9067a60044ea846f1f5e52e163c1",
 "privacy.html": "21c5409936d83278305d633ac9504302",
 "icons/Icon-maskable-512.png": "5992138a792363fd9c5a794a8d0864ac",
 "icons/Icon-192.png": "4b1127c8eacf5c7ee8ed9a210e443990",
